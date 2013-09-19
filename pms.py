fb98ff372bb29bbd6c1b360679a6de732ffb1a52
np1 <np1nagev@gmail.com>
2013-09-19 05:53:33 +0100

    Added license info

--- pms.py
+++ pms.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python
 
+''' pms 
+    Copyright (C)  2013 nagev
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.  '''
+
+__version__ = "0.02"
+__author__ = "nagev"
+__license__ = "GPLv3"
+
+
+
 import logging
 import time
 import json
