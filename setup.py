7e30977d1d81095762da766b484bb8a39cf13421
darren <np1nagev@gmail.com>
2013-12-14 16:07:19 +0000

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="Poor-Mans-Spotify",
-    version="0.18.1",
+    version="0.18.2",
     description="Search, Stream and Download MP3",
     keywords=["MP3", "music", "audio", "search", "stream", "download"],
     author="nagev",
