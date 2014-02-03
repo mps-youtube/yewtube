03ffe224583859e966cfef4b610272abe73bc676
darren <np1nagev@gmail.com>
2014-02-03 06:10:41 +0000

    fix start page

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name="pms-youtube",
-    version="0.01.02",
+    version="0.01.03",
     description="Terminal based YouTube jukebox with playlist management",
     keywords=["video", "music", "audio", "youtube", "stream", "download"],
     author="nagev",
