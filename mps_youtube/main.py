17516867b5b5ee81a159ef4e7d728fe36461096c
np1 <np1nagev@gmail.com>
2014-08-29 02:22:50 +1000

    Remove parameters to workaround broken gdata v2 api (gh-106)

--- mps_youtube/main.py
+++ mps_youtube/main.py
@@ -1869,6 +1869,12 @@ def usersearch(q_user, page=1, splash=True):
     else:
         user = q_user
         del query['q']
+
+        # issue # 106 - gdata v2 issue
+        # user video search fails with orderby and safeSearch parameters
+        del query['safeSearch']
+        del query['orderby']
+
         url = "https://gdata.youtube.com/feeds/api/users/%s/uploads" % user
         msg = "Video uploads by %s%s%s" % (c.y, user, c.w)
         failmsg = "User %s%s%s not found" % (c.y, user, c.w)
