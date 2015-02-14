3f1f5f5397f91527fe0940f69eb5cae1e9165c0f
np1 <np1nagev@gmail.com>
2015-02-14 12:39:56 +1100

    Add release date

--- mps_youtube/main.py
+++ mps_youtube/main.py
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
 
 __version__ = "0.2.2"
-__notes__ = "dev version"
+__notes__ = "released 14 Feb 2015"
 __author__ = "np1"
 __license__ = "GPLv3"
 
