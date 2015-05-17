d0e403bebcd084f208faf2f1387841e5b3350c2f
Ian D. Scott <ian@perebruin.com>
2015-05-17 08:00:17 -0700

    Remove predefined category_names
    Unneeded; should fix bug due to difference in format between original
    dict and generated one

--- mps_youtube/main.py
+++ mps_youtube/main.py
@@ -811,38 +811,7 @@ class g(object):
             "ignidx": "",
             "geo": "-geometry"}
         }
-    category_names = {"1": "Film & Animation",
-                      "2": "Autos & Vehicles",
-                      "10": "Music",
-                      "15": "Pets & Animals",
-                      "17": "Sports",
-                      "18": "Short Movies",
-                      "19": "Travel & Events",
-                      "20": "Gaming",
-                      "21": "Videoblogging",
-                      "22": "People & Blogs",
-                      "23": "Comedy",
-                      "24": "Entertainment",
-                      "25": "News & Politics",
-                      "26": "Howto & Style",
-                      "27": "Education",
-                      "28": "Science & Technology",
-                      "29": "Nonprofits & Activism",
-                      "30": "Movies",
-                      "31": "Anime/Animation",
-                      "32": "Action/Adventure",
-                      "33": "Classics",
-                      "34": "Comedy",
-                      "35": "Documentary",
-                      "36": "Drama",
-                      "37": "Family",
-                      "38": "Foreign",
-                      "39": "Horror",
-                      "40": "Sci-Fi/Fantasy",
-                      "41": "Thriller",
-                      "42": "Shorts",
-                      "43": "Shows",
-                      "44": "Trailers"}
+    category_names = {}
 
 def get_version_info():
     """ Return version and platform info. """
