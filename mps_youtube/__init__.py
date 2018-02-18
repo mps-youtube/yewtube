da798cf4f80513ebb424257e3fed9e9dd72c980b
Ian Douglas Scott <ian@iandouglasscott.com>
2018-02-17 20:54:43 -0800

    Version 0.2.8

--- mps_youtube/__init__.py
+++ mps_youtube/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.2.7.1"
-__notes__ = "released 6 July 2016"
+__version__ = "0.2.8"
+__notes__ = "released 17 February 2018"
 __author__ = "np1"
 __license__ = "GPLv3"
 __url__ = "https://github.com/mps-youtube/mps-youtube"
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info < (3,0):
 
 from setuptools import setup
 
-VERSION = "0.2.7.1"
+VERSION = "0.2.8"
 
 options = dict(
     name="mps-youtube",
