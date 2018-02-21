a97c234c20aba0ebeaab3cad1b33a964696a0aa3
Vishnunarayan K I <31964688+vn-ki@users.noreply.github.com>
2018-02-21 21:41:32 +0530

    Switch from bare except to IndexError and KeyError

--- mps_youtube/util.py
+++ mps_youtube/util.py
@@ -527,7 +527,9 @@ def _get_metadata_from_lastfm(artist, track):
         metadata['artist'] = data['track']['artist']['name']
         metadata['album'] = data['track']['album']['title']
         metadata['album_art_url'] = data['track']['album']['image'][-1]['#text']
-    except:
+    except KeyError:
+        return None
+    except IndexError:
         return None
 
     return metadata
