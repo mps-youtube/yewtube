35d78b9bdb68f1b0bb88483d17b3babe73aa05ea
Fabio Henrique <59309624+Deracinator@users.noreply.github.com>
2019-12-31 11:13:18 -0300

    Fix bug that overwrites previously created playlist of the same name. For more information see: https://github.com/mps-youtube/mps-youtube/issues/1046

--- mps_youtube/commands/local_playlist.py
+++ mps_youtube/commands/local_playlist.py
@@ -29,9 +29,11 @@ def playlist_remove(name):
 def playlist_add(nums, playlist):
     """ Add selected song nums to saved playlist. """
     nums = util.parse_multi(nums)
+    # Replacing spaces with hyphens before checking if playlist already exist.
+    # See https://github.com/mps-youtube/mps-youtube/issues/1046.
+    playlist = playlist.replace(" ", "-")
 
     if not g.userpl.get(playlist):
-        playlist = playlist.replace(" ", "-")
         g.userpl[playlist] = Playlist(playlist)
 
     for songnum in nums:
