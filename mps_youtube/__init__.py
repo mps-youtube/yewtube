f207c5d676169c39254935d03d61d7e63fef43ec
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-01-17 22:39:45 +0500

    manually bump version -> 1.0.3

--- mps_youtube/__init__.py
+++ mps_youtube/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "1.0.2"
-__notes__ = "released on 2 Jan 2022"
+__version__ = "1.0.3"
+__notes__ = "released on 17 Jan 2022"
 __author__ = "iamtalhaasghar"
 __license__ = "GPLv3"
 __url__ = "https://github.com/iamtalhaasghar/yewtube"
