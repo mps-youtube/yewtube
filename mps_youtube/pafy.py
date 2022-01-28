cdc0f01b300aad79900cba886eafdd1ed571f1d1
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-01-28 20:36:23 +0500

    remove output messages from yt-dlp

--- mps_youtube/pafy.py
+++ mps_youtube/pafy.py
@@ -1,8 +1,27 @@
-from youtubesearchpython import VideosSearch, StreamURLFetcher, Video
+from youtubesearchpython import VideosSearch
 import yt_dlp
 
+class MyLogger:
+    def debug(self, msg):
+        # For compatibility with youtube-dl, both debug and info are passed into debug
+        # You can distinguish them by the prefix '[debug] '
+        if msg.startswith('[debug] '):
+            pass
+        else:
+            self.info(msg)
+
+    def info(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
 def get_video_streams(ytid):
-    with yt_dlp.YoutubeDL() as ydl:
+    with yt_dlp.YoutubeDL({'logger':MyLogger()}) as ydl:
         info_dict = ydl.extract_info(ytid, download=False)
         return [i for i in info_dict['formats'] if i['format_note'] != 'storyboard']
 def video_search(query):
