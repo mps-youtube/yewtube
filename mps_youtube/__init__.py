762181e9f801265541ea7fe90ac8bdd9c060979e
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-01-28 15:25:46 +0500

    no module named setup

--- mps_youtube/__init__.py
+++ mps_youtube/__init__.py
@@ -1,5 +1,11 @@
-import setup
-__version__ = setup.get_version_number()
+def get_version_number():
+    f = open("VERSION")
+    version = None
+    for i in f.readlines():
+        if 'version' in i:
+            version = i.split()[-1].strip()
+    return version
+__version__ = get_version_number()
 __notes__ = "released on 28 Jan 2022"
 __author__ = "iamtalhaasghar"
 __license__ = "GPLv3"
