0316cc51c1ced163f51eae166c943331fb091bed
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-02-10 22:16:43 +0500

    added comments

--- mps_youtube/pafy.py
+++ mps_youtube/pafy.py
@@ -21,11 +21,21 @@ class MyLogger:
 
 
 def get_video_streams(ytid):
+
+    '''
+    given a youtube video id returns different video / audio stream formats' \
+    ''''
+
     with yt_dlp.YoutubeDL({'logger':MyLogger()}) as ydl:
         info_dict = ydl.extract_info(ytid, download=False)
         return [i for i in info_dict['formats'] if i.get('format_note') != 'storyboard']
 
 def download_video(ytid, folder):
+
+    '''
+    Given a youtube video id and target folder, this function will download video to that folder
+    '''
+
     ytdl_format_options = {
         'outtmpl': os.path.join(folder, '%(title)s-%(id)s.%(ext)s')
     }
@@ -35,21 +45,42 @@ def download_video(ytid, folder):
         return True
 
 def video_search(query):
+
+    '''
+    Given a keyword / query this function will return youtube video results against those keywords / query
+    '''
+
     videosSearch = VideosSearch(query, limit=50)
     wdata = videosSearch.result()['result']
     return wdata
 
 def channel_search(query):
+
+    '''
+    Search channel based on keyword / query provided by user
+    '''
+
     channelsSearch = ChannelsSearch(query, limit=50, region='US')
     return channelsSearch.result()['result']
 
 def playlist_search(query):
+
+    '''
+    Returns all playlists having similar names as keyword / query provided
+    '''
+
     playlistsSearch = PlaylistsSearch(query, limit=50)
     return playlistsSearch.result()['result']
 
 def get_playlist(playlist_id):
+
+    '''
+    Get all videos of a playlist identified by playlist_id
+    '''
+
     playlist = Playlist.get('https://www.youtube.com/playlist?list=%s' % playlist_id)
     return playlist
+
 def get_video_title_suggestions(query):
     suggestions = Suggestions(language = 'en', region = 'US')
     related_searches = suggestions.get(query)['result']
