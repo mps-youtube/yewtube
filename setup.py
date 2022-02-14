a7f0b66aa5663de4d7759dfa36c5b9e2b3146ea5
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-02-14 16:28:43 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ options = dict(
     download_url="https://github.com/iamtalhaasghar/yewtube/releases",
     packages=['mps_youtube', 'mps_youtube.commands', 'mps_youtube.listview', 'mps_youtube.players'],
     entry_points={'console_scripts': ['yt = mps_youtube:main.main']},
-    install_requires=requirements#['pyreadline','yt-dlp','youtube-search-python', 'pyperclip'],#open('requirements.txt').readlines(),
+    install_requires=requirements,#['pyreadline','yt-dlp','youtube-search-python', 'pyperclip'],#open('requirements.txt').readlines(),
     classifiers=[
         "Topic :: Utilities",
         "Topic :: Internet :: WWW/HTTP",
