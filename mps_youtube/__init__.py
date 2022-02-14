c0308bfb184568a396488694ebd7b778564a3cea
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-02-14 16:07:57 +0500

    Update __init__.py

--- mps_youtube/__init__.py
+++ mps_youtube/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "1.1.4"
-__notes__ = "released on 10 Feb 2022"
+__version__ = "1.1.5"
+__notes__ = "released on 14 Feb 2022"
 __author__ = "iamtalhaasghar"
 __license__ = "GPLv3"
 __url__ = "https://github.com/iamtalhaasghar/yewtube"
