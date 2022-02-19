eabfb5233c7b87c5f300ebc41250a3f52db07411
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-02-19 10:49:58 +0500

    fix: check for app updates

--- mps_youtube/commands/misc.py
+++ mps_youtube/commands/misc.py
@@ -60,9 +60,9 @@ def quits(showlogo=True):
     if config.CHECKUPDATE.get and showlogo:
 
         try:
-            url = "https://raw.githubusercontent.com/mps-youtube/mps-youtube/master/VERSION"
+            url = "https://raw.githubusercontent.com/iamtalhaasghar/yewtube/master/setup.py"
             v = urlopen(url, timeout=1).read().decode()
-            v = re.search(r"^version\s*([\d\.]+)\s*$", v, re.MULTILINE)
+            v = re.search(r'__version__\s*=\s*"\s*([\d\.]+)\s*"\s*', v, re.MULTILINE)
 
             if v:
                 v = v.group(1)
